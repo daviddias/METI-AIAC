f87a338b274b86ae9c52edcc6eb261e9713f8c2c
David Dias <daviddias.p@gmail.com>
2013-03-22 08:08:37 +0000

    Added Report and Performance Analysis

--- AES_AIAC/src/aes/AESMain.java
+++ AES_AIAC/src/aes/AESMain.java
@@ -73,73 +73,52 @@ public class AESMain{
 			blockCypherMode = BlockCypherMode.ECB;
 		}
 		
-		/*
-		AES_API aes_api = new AES_API();
-		aes_api.init(cypherMode, blockCypherMode, key);
-		//aes.test();
-		byte[] input = FileRW.readFile(inputFilePath);
-		//aes_api.update(input);
-		byte[] output = aes_api.doFinal(input);
-		*&
+	
 		
 		/**
+		 * ------------------------------------------------
 		 * Testing
 		 */
-		
+	
 		
 		AES_API aes_api_encrypt = new AES_API();
 		aes_api_encrypt.init(CypherMode.ENCRYPT, BlockCypherMode.ECB, key);
 		byte[] input = FileRW.readFile(inputFilePath);
-		byte[] CypherText = aes_api_encrypt.doFinal(input);
+		long start = System.currentTimeMillis();
 		
+		
+		byte[] CypherText = aes_api_encrypt.doFinal(input);
+		/*
 		try {
 			System.out.println("Input PlainText was:");
 			System.out.println(new String(input, "UTF-8"));
 			System.out.println("CypherText was:");
 			System.out.println(new String(CypherText, "UTF-8"));
 		} catch (UnsupportedEncodingException e) {System.out.println("FUCK");}
-
+		*/
 		
-		System.out.println("Going to decrypt");
+		//System.out.println("Going to decrypt");
 		
 		AES_API aes_api_decrypt = new AES_API();
 		aes_api_decrypt.init(CypherMode.DECRYPT, BlockCypherMode.ECB, key);
 		
 		byte[] outputPlainText = aes_api_decrypt.doFinal(CypherText);
-		
+		/*
 		try {
 			System.out.println("CypherText was:");
 			System.out.println(new String(CypherText, "UTF-8"));
 			System.out.println("Output PlainText was:");
 			System.out.println(new String(outputPlainText, "UTF-8"));
 		} catch (UnsupportedEncodingException e) {System.out.println("FUCK");}
-		
+		*/
 		System.out.println("\n Finito \n");
 		
+		long end = System.currentTimeMillis();
+		System.out.println(end-start);
 		
 		/*
-		byte[] input = FileRW.readFile(inputFilePath);
-		
-		System.out.println("Plaintext is:");
-		System.out.println(input);
-		try {
-			System.out.println(new String(input, "UTF-8"));
-		} catch (UnsupportedEncodingException e) {System.out.println("FUCK");}
-
-		System.out.println("Testing ECB Encryption");
-		byte[] cypheredText = AESTAKENFROMTHEWEB.encrypt(input,key);
-		System.out.println(cypheredText);
-		System.out.println("Encryption Complete");
-
-		System.out.println("Testing ECB Decryption");
-		byte[] plainText = AESTAKENFROMTHEWEB.decrypt(cypheredText,key);
-		System.out.println(plainText);
-		try {
-			System.out.println(new String(plainText, "UTF-8"));
-		} catch (UnsupportedEncodingException e) {System.out.println("FUCK");}
-		System.out.println("Decryption Complete");
-		
-		*/
+		 * ------------------------------------------------
+		 */
 		
 	}
 
